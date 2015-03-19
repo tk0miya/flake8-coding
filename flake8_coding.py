4a74984bcebcf41354ee8bfc5168624e37ea21e0
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-03-19 13:58:49 +0900

    Initial commit

---
+++ flake8_coding.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+__version__ = '0.1.0'
+
+
+class CodingChecker(object):
+    name = 'flake8_coding'
+    version = __version__
+
+    def __init__(self, tree, filename):
+        self.filename = filename
+
+    @classmethod
+    def add_options(cls, parser):
+        parser.add_option(
+            '--accept-encodings', default='latin-1, utf-8', action='store',
+            help="Acceptable source code encodings for `coding:` magic comment"
+        )
+        parser.config_options.append('accpet-encodings')
+
+    @classmethod
+    def parse_options(cls, options):
+        cls.encodings = [e.strip().lower() for e in options.accept_encodings.split(',')]
+
+    def run(self):
+        with open(self.filename) as f:
+            # PEP-263 says: a magic comment must be placed into the source
+            #               files either as first or second line in the file
+            for lineno in range(1, 3):
+                matched = re.search('coding: (\S+)', f.readline(), re.IGNORECASE)
+                if matched:
+                    if matched.group(1).lower() not in self.encodings:
+                        yield lineno, 0, "C102 Unknown encoding found in coding magic comment", type(self)
+                    break
+            else:
+                yield 0, 0, "C101 Coding magic comment not found", type(self)
---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='flake8-coding',
+    version='0.1.0',
+    description='Adds coding magic comment checks to flake8',
+    long_description=open("README.rst").read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+    ],
+    author='Takeshi KOMIYA',
+    author_email='i.tkomiya at gmail.com',
+    url='https://github.com/tk0miya/flake8-coding',
+    license='Apache License 2.0',
+    keywords='pep8 flake8 coding',
+    py_modules=['flake8_coding'],
+    install_requires=[
+        'setuptools',
+        'flake8',
+    ],
+    entry_points={
+        'flake8.extension': ['C10 = flake8_coding:CodingChecker'],
+    },
+)
